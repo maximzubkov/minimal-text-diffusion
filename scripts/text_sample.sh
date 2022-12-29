4b910339ca4821add791570923ec30121286a6c7
maximzubkov <zubkov.md@phystech.edu>
2022-12-29 12:28:51 +0100

    Move text sample

--- scripts/text_sample.sh
+++ scripts/text_sample.sh
@@ -20,7 +20,7 @@ CLAMP=${7:-no_clamp}
 SEQ_LEN=${8:-10}
 SEED=${9:-10708}
 
-python -u src/train_infer/text_sample.py --model_name_or_path ${MODEL_NAME} \
+python -u text_sample.py --model_name_or_path ${MODEL_NAME} \
 --batch_size ${BATCH_SIZE} --num_samples ${NUM_SAMPLES} --top_p ${TOP_P} \
 --seed ${SEED} \
 --out_dir ${OUT_DIR} --diffusion_steps ${DIFFUSION_STEPS} --clamp ${CLAMP} --sequence_len ${SEQ_LEN}
