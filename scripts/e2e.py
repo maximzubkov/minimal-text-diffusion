955007ffb5d7f5b3cca69a12cde85ef4b00cf301
maximzubkov <zubkov.md@phystech.edu>
2022-12-29 11:21:54 +0100

    Fix

--- scripts/e2e.py
+++ scripts/e2e.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     data_path = join("data", "e2e")
     for split in ["train", "test", "valid"]:
         input_path = join(data_path, f"{split}_initial.txt")
-        output_path = join(data_path, f"e2e-{split}.txt")
+        output_path = join("data", f"e2e-{split}.txt")
         with open(input_path, "r") as f, open(output_path, "w") as g:
             for line in f:
                 g.write(line.split("||")[-1])
